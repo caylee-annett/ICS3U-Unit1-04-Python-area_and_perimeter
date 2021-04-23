2b57f46d0d23f02816befae2884fb0aaa271dd01
Caylee Annett <caylee.annett@mths.ca>
2021-04-23 18:20:46 +0000

    added area_and_perimeter

---
+++ area_and_perimeter.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+# Created by: Caylee Annett
+# Created on: April 2021
+# This program calculates the area and perimeter of a rectangle with
+#   dimensions 5cm x 3cm
+
+
+def main():
+    # This function calculates the area and perimeter
+    print("If a rectangle has dimensions:")
+    print("5cm x 3cm")
+    print("")
+    print("Area is {}cm^2.".format(5*3))
+    print("Perimeter is {}cm.".format(2*(5+3)))
+
+
+if __name__ == "__main__":
+    main()
